9c011cc6ba4ba2fb44b903c52dfcea26f9794cd6
Chip <chip.elmer@gmail.com>
2018-07-26 10:17:50 -0500

    Add project files.

---
+++ Mark4Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mark4Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
